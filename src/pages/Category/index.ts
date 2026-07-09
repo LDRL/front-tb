910bd75dbaa471e172e549597d9949bd282ab14e
lreyes <lreyes@homeland.com.gt>
2026-07-08 21:09:16 -0600

    Fix Category type filename casing

--- src/pages/Category/index.ts
+++ src/pages/Category/index.ts
@@ -2,6 +2,6 @@ export * from './adapter/category.adapter';
 export * from './components/index';
 export * from './components/CategoryCreate/index';
 export * from './components/CategoryTable/index';
-export * from './models/Category.type';
+export * from './models/index';
 export { default as CategoryPage } from './CategoryPage';
 export * from './components/Header/index';
