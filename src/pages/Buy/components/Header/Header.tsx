f1d440514801e440031e6ab4bfca8fddc9a08c1c
lreyes <lreyes@homeland.com.gt>
2026-07-08 20:45:35 -0600

    fix header buy

--- src/pages/Buy/components/Header/Header.tsx
+++ src/pages/Buy/components/Header/Header.tsx
@@ -1,50 +1,18 @@
-import React, { useCallback, useEffect } from 'react';
+import React from 'react';
 import { Button } from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux';
-import { FormInputText } from '@/components';
-import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-import { setSearchBrand } from '@/redux/brandSlice';
 
-import debounce from 'just-debounce-it';
 import "./Header.css"
 
 const Header: React.FC = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const search = useSelector((state: any) => state.buy.search);
-
-  const debouncedGetBrands = useCallback(debounce((search: string) =>{
-    dispatch(setSearchBrand(search));
-  },300 ),[])
 
   const handleClick = () => {
     navigate("create")
   };
 
-  const { control, reset } = useForm({
-    defaultValues: { search },
-  });
-
-  useEffect(() => {
-    reset({ search });
-  }, [search, reset]);
-
-  const handleSearchChange = (value: string) => {
-    debouncedGetBrands(value)
-  };
-
   return (
     <div className='header'>
-      <div style={{ alignItems: "right" }}>
-        <FormInputText
-          name="search"
-          control={control}
-          label="Buscar"
-          externalOnChange={handleSearchChange}
-        />
-      </div>
-
       <div>
         <Button variant="contained" color="primary" onClick={handleClick}>
           Nueva compra
