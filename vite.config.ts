307f3fd21a7ee5c0d9bd7e28a80a29aa8d10fbbf
ldrl <drdanielreyes5@gmail.com>
2026-09-10 17:23:18 -0600

    feat: add vite preview

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,30 @@
 import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import react from '@vitejs/plugin-react-swc'
-// import * as path from 'path'
 import * as path from 'path'
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ...(process.env.ANALYZE
+      ? [
+          visualizer({
+            open: true,
+            filename: './dist/stats.html',
+            gzipSize: true,
+            brotliSize: true,
+          }),
+        ]
+      : []),
+  ],
+
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
-  }
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
 })
-
-
-
-
-
